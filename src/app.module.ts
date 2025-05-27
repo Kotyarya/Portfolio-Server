cd8cdb2dad42dc78cf908d9f923172b5355d6dfa
Kotyarya <sunguuner500@gmail.com>
2025-05-27 23:22:19 +0200

    Setup database connection and Create base entities #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { BlocksModule } from './blocks/blocks.module';
-import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [BlocksModule],
