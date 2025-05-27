a068d9e35c2287dafb9d73d80e3f1a475ec3b542
Kotyarya <sunguuner500@gmail.com>
2025-05-27 23:21:55 +0200

    Setup database connection and Create base entities #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BlocksModule } from './blocks/blocks.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [BlocksModule],
--- src/blocks/blocks.service.ts
+++ src/blocks/blocks.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class BlocksService {
-    getStartPage():string {
-        return "Start Page";
-    }
+  constructor(private readonly prisma: PrismaService) {}
+
+  getStartPage(): any {
+    return this.prisma.blocks.findFirst({
+      where: {
+        block_type: 'start_page',
+      },
+      select: {
+        data: true,
+      },
+    });
+  }
 }
