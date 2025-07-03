3a6fc2a23785f78f3ff8294dfeb876f1a527b4da
Kotyarya <sunguuner500@gmail.com>
2025-07-03 17:48:35 +0200

    Created projects model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlocksModule } from './blocks/blocks.module';
 import { MediaModule } from './media/media.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [BlocksModule, MediaModule],
+  imports: [BlocksModule, MediaModule, ProjectsModule],
   controllers: [],
   providers: [],
 })
