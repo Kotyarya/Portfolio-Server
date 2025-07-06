4f78190eee7375a7f44468b7a402cccede02bba6
Kotyarya <sunguuner500@gmail.com>
2025-07-06 11:38:03 +0200

    Created Skills model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BlocksModule } from './blocks/blocks.module';
 import { MediaModule } from './media/media.module';
 import { ProjectsModule } from './projects/projects.module';
+import { SkillsModule } from './skills/skills.module';
 
 @Module({
-  imports: [BlocksModule, MediaModule, ProjectsModule],
+  imports: [BlocksModule, MediaModule, ProjectsModule, SkillsModule],
   controllers: [],
   providers: [],
 })
