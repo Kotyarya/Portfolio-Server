613965e7a5b4fe276bae3e45fbdb020811375943
Kotyarya <sunguuner500@gmail.com>
2025-10-24 22:58:48 +0200

    refactoring

--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,21 +1,21 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { join } from 'path';
-import { createReadStream, existsSync } from 'fs';
-import { Writable } from 'stream';
-import { Response } from 'express';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {join} from 'path';
+import {createReadStream, existsSync} from 'fs';
+import {Writable} from 'stream';
+import {Response} from 'express';
 
 @Injectable()
 export class MediaService {
-  private readonly mediaPath = join(__dirname, '..', '..', 'static_media');
+    private readonly mediaPath = join(__dirname, '..', '..', 'static');
 
-  getFile(fileName: string, res: Response & Writable): void {
-    const path = join(this.mediaPath, fileName);
+    getFile(fileName: string, res: Response & Writable): void {
+        const path = join(this.mediaPath, fileName);
 
-    if (!existsSync(path)) {
-      throw new NotFoundException('File not found');
+        if (!existsSync(path)) {
+            throw new NotFoundException('File not found');
+        }
+
+        const stream = createReadStream(path);
+        stream.pipe(res);
     }
-
-    const stream = createReadStream(path);
-    stream.pipe(res);
-  }
 }
