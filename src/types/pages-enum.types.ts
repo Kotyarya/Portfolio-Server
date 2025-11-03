9331d62d8111a50e3c5ba08573edcd7e1128edb1
Kotyarya <sunguuner500@gmail.com>
2025-11-03 16:11:49 +0100

    refactoring

--- src/types/pages-enum.types.ts
+++ src/types/pages-enum.types.ts
@@ -2,5 +2,5 @@ export enum PagesEnum {
     HERO_BLOCK = 'hero',
     ABOUT_ME_BLOCK = 'about_me',
     SKILLS_PREVIEW_BLOCK = 'skills_preview',
-    PROJECTS_PAGE = 'projects',
+    PROJECTS_PREVIEW_BLOCK = 'projects_preview',
 }
