301ad48458ea904da35e1e623d134176c2e9ce1f
Kotyarya <sunguuner500@gmail.com>
2025-11-21 22:55:13 +0100

    Refactoring

--- src/types/pages-enum.types.ts
+++ src/types/pages-enum.types.ts
@@ -4,4 +4,7 @@ export enum PagesEnum {
     SKILLS_PREVIEW_BLOCK = 'skills_preview',
     PROJECTS_PREVIEW_BLOCK = 'projects_preview',
     CONTACT_ME_BLOCK = 'contact_me',
+    BEYOND_CODE_BLOCK = 'beyond_code',
+    WHATS_NEXT_BLOCK = 'whats_next',
+    CERTIFICATES_BLOCK = 'certificates'
 }
