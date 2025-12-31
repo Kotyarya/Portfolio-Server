04f513b64cea3fb9f335202454875a42e25a7940
Kotyarya <sunguuner500@gmail.com>
2025-12-31 02:42:14 +0100

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { join } from 'path';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {join} from 'path';
 import * as express from 'express';
-import { AllExceptionsFilter } from './common/AllExceptionsFilter.filter';
-import { ValidationPipe } from '@nestjs/common';
+import {AllExceptionsFilter} from './common/AllExceptionsFilter.filter';
+import {ValidationPipe} from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.use('/static_media', express.static(join(__dirname, '..', 'media')));
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.use('/static_media', express.static(join(__dirname, '..', 'media')));
 
-  await app.listen(process.env.PORT ?? 5000);
+    await app.listen(process.env.PORT ?? 5000, "0.0.0.0");
 }
+
 bootstrap();
